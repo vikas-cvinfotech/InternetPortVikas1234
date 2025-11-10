20780795a2cc9de479511839cb9cfbeba52497c1
Vikas <vikas@cvinfotech.com>
2025-11-10 18:04:03 +0530

    correct bg color

--- src/sections/SupportSection.jsx
+++ src/sections/SupportSection.jsx
@@ -12,40 +12,16 @@ export default function SupportSection() {
   const lighterAccentColor = 'rgb(190 24 35)'; // Using same accent color as no lighter variant is defined
 
   return (
-    <div className="relative bg-secondary">
-      <div className="relative h-80 overflow-hidden bg-accent md:absolute md:left-0 md:h-full w-full">
+    <div className="relative">
+      <div className="relative h-80 overflow-hidden  md:absolute md:left-0 md:h-full w-full">
         <Image
           alt=""
-          src="https://internetportcom.b-cdn.net/se/img/kundtjanst-i-telefon.webp?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=60"
+          src="https://internetportcom.b-cdn.net/se/img/helpimagetwo.webp"
           fill
           sizes="(max-width: 768px) 100vw, 50vw"
           className="object-cover"
         />
-        <div className="absolute inset-0 bg-[#1d1d1dd6]" />
-        <svg
-          viewBox="0 0 926 676"
-          aria-hidden="true"
-          className="absolute -bottom-24 left-24 w-[57.875rem] transform-gpu blur-[118px]"
-        >
-          <path
-            d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z"
-            fill="url(#custom-gradient-support)"
-            fillOpacity=".4"
-          />
-          <defs>
-            <linearGradient
-              id="custom-gradient-support"
-              x1="926.392"
-              x2="-109.635"
-              y1=".176"
-              y2="321.024"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor={lighterAccentColor} />
-              <stop offset={1} stopColor={accentColor} />
-            </linearGradient>
-          </defs>
-        </svg>
+        <div className="absolute inset-0 bg-[#1D1D1D]/[0.14] " />
       </div>
       <div className="relative mx-auto py-16 sm:py-24 lg:px-8 ">
         <div className="text-center px-4 sm:px-[80px] xxl:px-[135px]">
