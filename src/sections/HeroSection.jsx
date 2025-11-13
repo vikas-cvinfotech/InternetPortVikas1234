c3d2b9d202ed9ca874f00d9e694ee4fe159430f7
Vikas <vikas@cvinfotech.com>
2025-11-13 15:34:32 +0530

    fix the animation after click

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -9,10 +9,18 @@ export default function HeroSection() {
   const t = useTranslations('hero');
   const [isImageVisible, setIsImageVisible] = useState(true); // Add this state
 
+  const handleClick = () => {
+    setIsImageVisible(false);
+    setTimeout(() => setIsImageVisible(true), 1000);
+  };
+
   return (
     <div className="relative">
       <Container className="flex flex-col justify-center pt-5 pb-0 text-center sm:pt-20 sm:pb-0 lg:pt-16 relative">
-        <div className="group flex flex-col justify-center relative z-[2] hover:cursor-pointer clickable-div">
+        <div
+          className="group flex flex-col justify-center relative z-[2] hover:cursor-pointer clickable-div"
+          onClick={handleClick}
+        >
           <h1 className="relative test-bg hover:cursor-pointer  mx-auto max-w-4xl font-display text-[1.8rem] xs:text-3xl font-semibold tracking-tight text-secondary xs:text-4xl sm:text-5xl md:text-6xl lg:text-[64px] capitalize">
             {t('hassleFree')}{' '}
             <span className="relative font-bold whitespace-nowrap bg-[radial-gradient(circle_at_center,_#BE1823_0%,_#F3404C_47.17%,_#9C1821_100%)] bg-clip-text text-transparent bg-[length:200%_200%] animate-gradient">
