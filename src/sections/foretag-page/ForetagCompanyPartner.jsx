153a90dd22f993b3675b85ff287f1932401e4e33
Vikas <vikas@cvinfotech.com>
2025-12-16 17:44:39 +0530

    correct gap

--- src/sections/foretag-page/ForetagCompanyPartner.jsx
+++ src/sections/foretag-page/ForetagCompanyPartner.jsx
@@ -20,7 +20,7 @@ export default function ForetagCompanyPartner({
       <div className="px-4 sm:px-[50px] xl:px-[80px] xxl:px-[135px]">
         <div className="grid grid-cols-1 lg:grid-cols-12 items-start justify-center gap-4 gap-y-6">
           <div
-            className={`flex flex-col gap-[30] ${somestyle1} ${
+            className={`flex flex-col gap-30 ${somestyle1} ${
               gridJustifyCenter ? gridJustifyCenter : ''
             }`}
           >
