c276f88d5c182eaa4fb2f00b0d77704f9083935f
Vikas <vikas@cvinfotech.com>
2026-01-07 12:16:31 +0530

    complete plesk page convertion

--- src/app/[locale]/foretag/hosting/webb-hosting/konfigurera-plesk/page.jsx
+++ src/app/[locale]/foretag/hosting/webb-hosting/konfigurera-plesk/page.jsx
@@ -11,29 +11,30 @@ import StorageCard from '@/components/StorageCard';
 import { features, PleskFaq, featuresIncluded, pleskCardData } from '@/components/plesk/PleskData';
 
 export default function PleskPage() {
-  const { locale } = useParams();
-  const t = useTranslations('telephony');
+  const t = useTranslations('pleskPage');
+  const getPleskFeatures = featuresIncluded(t);
+  const getPleskCardData = pleskCardData(t);
 
   return (
     <div className="bg-primary text-secondary">
       <CommonBanner
         imageAlt="sakerhet-cybersakerhet-online"
         imageSrc="https://internetportcom.b-cdn.net/se/img/sakerhet-cybersakerhet-online.jpg"
-        titlePart1="Plesk webhosting"
-        desc="Plesk is a popular web hosting control panel which offers next-level management of websites and domains from a single dashboard."
+        titlePart1={t('commonBanner.title')}
+        desc={t('commonBanner.desc')}
         link="https://portal.internetport.com/cart/webbhotell/"
-        linkLabel="Choose Package"
+        linkLabel={t('commonBanner.linkLabel')}
       />
       <ContentBlock
-        title="Plesk webhosting configurations"
-        desc="We provide the latest stable version of Plesk on our cloud web hosting platform for customers to manage their online presence."
+        title={t('contentBlock.title')}
+        desc={t('contentBlock.desc')}
         imageUrl="https://internetportcom.b-cdn.net/se/img/plesk-webbhotell-server.jpg"
         alt="plesk-webbhotell-server"
         padd="pt-24 pb-[60px]"
       />
       <OfferCard
-        title="Features & benefits"
-        offerData={features}
+        title={t('moreFeatures.title')}
+        offerData={features(t)}
         bgImage="https://internetportcom.b-cdn.net/se/img/kross-transparent-bakgrund.webp"
         gridColClass="sm:grid-cols-2 xl:grid-cols-4"
         zIndex="z-[0]"
@@ -42,43 +43,40 @@ export default function PleskPage() {
       />
       <div className="px-4 sm:px-[50px] xl:px-[80px] xxl:px-[135px] flex flex-col gap-30 py-24 mb-1">
         <div>
-          <h2 className="text-3xl font-bold text-darkGray mb-4">Plesk Obsidian Features</h2>
-          <p className="text-base text-lightergray">
-            The complete set of tools to build, secure and run your website or applications.
-          </p>
+          <h2 className="text-3xl font-bold text-darkGray mb-4">{t('pleskFeatures.mainTitle')}</h2>
+          <p className="text-base text-lightergray">{t('pleskFeatures.mainSubtitle')}</p>
         </div>
         <div className="flex flex-col gap-30">
-          {featuresIncluded &&
-            featuresIncluded.map((item, index) => {
-              const isStringIcon = typeof item.icon === 'string';
-              const ComponentIcon = isStringIcon ? null : item.icon;
-              return (
-                <div key={index} className="flex items-start gap-8 rounded-lg bg-primary h-full">
-                  <dt className="font-semibold">
-                    <div className="rounded-md inline-flex items-center bg-surfaceSecondary p-5">
-                      {isStringIcon ? (
-                        <div dangerouslySetInnerHTML={{ __html: item.icon }} />
-                      ) : (
-                        ComponentIcon && <ComponentIcon />
-                      )}
-                    </div>
-                  </dt>
-                  <dd className="flex flex-auto flex-col gap-2">
-                    <h2 className="text-xl font-bold text-darkGray">{item.title}</h2>
-                    <p className="text-base font-normal text-paraSecondary">{item.description}</p>
-                  </dd>
-                </div>
-              );
-            })}
+          {getPleskFeatures.map((item, index) => {
+            const isStringIcon = typeof item.icon === 'string';
+            const ComponentIcon = isStringIcon ? null : item.icon;
+            return (
+              <div key={index} className="flex items-start gap-8 rounded-lg bg-primary h-full">
+                <dt className="font-semibold">
+                  <div className="rounded-md inline-flex items-center bg-surfaceSecondary p-5">
+                    {isStringIcon ? (
+                      <div dangerouslySetInnerHTML={{ __html: item.icon }} />
+                    ) : (
+                      ComponentIcon && <ComponentIcon />
+                    )}
+                  </div>
+                </dt>
+                <dd className="flex flex-auto flex-col gap-2">
+                  <h2 className="text-xl font-bold text-darkGray">{item.title}</h2>
+                  <p className="text-base font-normal text-paraSecondary">{item.description}</p>
+                </dd>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className="bg-secondary relative z-[0]">
         <div className="px-4 sm:px-[50px] xl:px-[80px] xxl:px-[135px] py-24">
           <div className="text-center mb-[60px]">
-            <h1 className="text-[32px] text-primary font-bold">Plesk configurations</h1>
+            <h1 className="text-[32px] text-primary font-bold">{t('pleskConfig.mainTitle')}</h1>
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-30 gap-y-16">
-            {pleskCardData.map((data, index) => (
+            {getPleskCardData.map((data, index) => (
               <StorageCard
                 key={index}
                 title={data.title}
@@ -94,16 +92,16 @@ export default function PleskPage() {
       </div>
 
       <FaqSection
-        title="Frequently asked questions"
-        faqs={PleskFaq}
+        title={t('pleskFaq.title')}
+        faqs={PleskFaq(t)}
         image="https://internetportcom.b-cdn.net/se/img/faq-support-kundservice.jpg"
         alt="faq-support-kundservice"
       />
       <AdvisorContactCard
-        title="If you are interested in Plesk"
-        desc="Get in touch with us for more information and let us help you."
+        title={t('advisorContact.title')}
+        desc={t('advisorContact.desc')}
         link="https://portal.internetport.com/cart/webbhotell/"
-        linkLabel="Get Started"
+        linkLabel={t('advisorContact.linkLabel')}
         gap="gap-[20]"
         marginBottom=" "
       />
